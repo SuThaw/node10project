7631cd083fc39f60377135e0947f81599d06f7d7
Su Thaw <suthaw524@gmail.com>
2015-06-14 12:21:56 +0800

    Validation Finsihed

--- user_login_system/routes/users.js
+++ user_login_system/routes/users.js
@@ -15,4 +15,46 @@ router.get('/login',function  (req,res,next) {
 	res.render('login',{title:'LogIn'});
 });
 
+router.post('/register',function(req,res,next){
+	var name = req.body.name,
+	    email = req.body.email,
+	    username = req.body.username,
+            password = req.body.password,
+            cpassword = req.body.cpassword;
+	if(req.files.profileImage){
+		var profileImageOriginalName = req.files.profileImage.originalName;
+		var profileImageName = req.files.profileImage.name;
+		var profileImageMime = req.files.profileImage.mimetype;
+		var profileImagePath = req.files.profileImage.path;
+		var profileImageExt = req.files.profileImage.extension;
+		var profileImageSize = req.files.profileImage.size;
+	}else{
+		var profileImageName = 'noimage.jpg';
+	}	
+	
+	//Form validation
+	req.checkBody('name','Name Field is required').notEmpty();
+	req.checkBody('email','Email Field is required').notEmpty();
+	req.checkBody('email','Email is not valid').isEmail();
+	req.checkBody('username','User Name Field is required').notEmpty();
+	req.checkBody('password','Password Field is required').notEmpty();
+	req.checkBody('cpassword','Password do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if (errors) {
+		res.render('register',{
+			errors:errors,
+			name:name,
+			email:email,
+			username:username,
+			password:password,
+			cpassword:cpassword
+		});
+	}else{
+
+	
+	}
+});
+
 module.exports = router;
