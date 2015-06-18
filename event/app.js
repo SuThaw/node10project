90f7fec845d1fb3138707503168137bb82a0698b
Su Thaw <suthaw524@gmail.com>
2015-06-18 12:28:35 +0800

    Drywall install

--- event/app.js
+++ event/app.js
@@ -1,60 +1,95 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//dependencies
+var config = require('./config'),
+    express = require('express'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    mongoStore = require('connect-mongo')(session),
+    http = require('http'),
+    path = require('path'),
+    passport = require('passport'),
+    mongoose = require('mongoose'),
+    helmet = require('helmet'),
+    csrf = require('csurf');
 
+//create express app
 var app = express();
 
-// view engine setup
+//keep reference to config
+app.config = config;
+
+//setup the web server
+app.server = http.createServer(app);
+
+//setup mongoose
+app.db = mongoose.createConnection(config.mongodb.uri);
+app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
+app.db.once('open', function () {
+  //and... we have a data store
+});
+
+//config data models
+require('./models')(app, mongoose);
+
+//settings
+app.disable('x-powered-by');
+app.set('port', config.port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//middleware
+app.use(require('morgan')('dev'));
+app.use(require('compression')());
+app.use(require('serve-static')(path.join(__dirname, 'public')));
+app.use(require('method-override')());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(config.cryptoKey));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.cryptoKey,
+  store: new mongoStore({ url: config.mongodb.uri })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(csrf({ cookie: { signed: true } }));
+helmet(app);
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+//response locals
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.cookie('_csrfToken', req.csrfToken());
+  res.locals.user = {};
+  res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl();
+  res.locals.user.username = req.user && req.user.username;
+  next();
 });
 
-// error handlers
+//global locals
+app.locals.projectName = app.config.projectName;
+app.locals.copyrightYear = new Date().getFullYear();
+app.locals.copyrightName = app.config.companyName;
+app.locals.cacheBreaker = 'br34k-01';
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+//setup passport
+require('./passport')(app, passport);
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+//setup routes
+require('./routes')(app, passport);
+
+//custom (friendly) error handler
+app.use(require('./views/http/index').http500);
+
+//setup utilities
+app.utility = {};
+app.utility.sendmail = require('./util/sendmail');
+app.utility.slugify = require('./util/slugify');
+app.utility.workflow = require('./util/workflow');
+
+//listen up
+app.server.listen(app.config.port, function(){
+  //and... we're live
+  console.log('Server is running on port ' + config.port);
 });
-
-
-module.exports = app;
